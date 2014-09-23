6d264198b123337092ccead049e4c8c06a54683c
Matthew Vasseur <mattvasseur@gmail.com>
2014-09-23 13:28:03 -0400

    hw1

--- homework/week1/script.js
+++ homework/week1/script.js
@@ -1,9 +1,25 @@
-console.log("javascript working!"); // Just to test. 
+console.log("javascript working!"); // Just to test.
 $(document).ready(function() {
-  $.get("http://www.reddit.com/hot.json", function(response) {
+  populate("http://www.reddit.com/hot.json");
+
+  $("#hot").click(function() {
+    populate("http://www.reddit.com/hot.json");
+  });
+  $("#new").click(function() {
+    populate("http://www.reddit.com/new.json");
+  });
+  $("#petitions").click(function() {
+    populate("https://www.api.whitehouse.gov/v1/petitions.json");
+  });
+});
+
+//puts in data
+var populate = function (link) {
+  $.get(link, function(response) {
     // If you don't understand line 6, you may want to read up
-    // on Javascript objects, in the slides or internet. 
+    // on Javascript objects, in the slides or internet.
     var stories = response.data.children;
+    $("#list").empty(); // clear list
     for(var i in stories) {
       story = stories[i].data;
       var elem = $("<li></li>");
@@ -11,10 +27,19 @@ $(document).ready(function() {
       // In the data in the Javascript object 'story',
       // find the title, permalink, and upvote count
       // Then create HTML elements with jQuery (like in line 9)
-      // and append them to the #list element. 
+      // and append them to the #list element.
+
+
       $("#list").append(elem);
+      $("#list").append("<p>");
+      $("#list").append("<a href='http://reddit.com"+story.permalink+"'>"+story.title+"</a>");
+      $("#list").append(" (" + story.score + " points)</p>");
+      $("#list").append("<img src='" + story.thumbnail +"'>");
+
+
       // Look at the JS console in Chrome to see what story looks like
       console.log(story);
     }
   })
-});
+};
+
