0c1cb939d5365011a134dfe1c0a1eecbbaff26de
Matthew Vasseur <mattvasseur@gmail.com>
2014-09-30 20:31:07 -0400

    class 3

---
+++ classwork/week3/angle.js
@@ -0,0 +1,10 @@
+var helloAngular = angular.module("HelloAngular", []);
+
+helloAngular.controller("HelloCtrl", function($scope) {
+
+  $scope.logText= function() {
+    console.log($scope.myText);
+    //$scope.myText += "STOP!";
+  };
+
+});
